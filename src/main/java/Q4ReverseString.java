72b3d2f4e1069e4add0721457f24849418bc3699
kazrepo <kj944108@wcupa.edu>
2024-09-08 11:41:03 -0400

    Q4 fix

--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -4,6 +4,9 @@ public class Q4ReverseString {
 
     public static String reverse(String s) {
         // TODO: Implement the recursive function to reverse the string
+        if (s.length() == 0) {
+            return s;
+        }
         return reverse(s.substring(1)) + s.charAt(0);
     }
 }
